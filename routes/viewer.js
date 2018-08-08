a5f39749f79945d3635cfac610cd064a61e72f29
RenanSchmitt <renan@dzestudio.com.br>
2018-08-08 11:49:57 -0300

    Add logic slug and mocha

--- routes/viewer.js
+++ routes/viewer.js
@@ -3,13 +3,50 @@ const fs = require('fs');
 var express = require('express');
 var router = express.Router();
 
+var products = [];
 
-router.get('/', function(req, res, next) {
+products = [   
+  {   
+    "name" : "Chápeu",
+    "slug" : "chapeu",
+    "image" : "images/chapeu.jpg",
+    "descricao" : "É um chapéu muito bonito.",
+    "valor" : 150.00,   
+    "tamanho" : "25",
+  },
+  {    
+    "name" : "Flanela",
+    "slug" : "flanela",    
+    "image" : "images/chapeu.jpg",
+    "descricao" : "É um chapéu muito bonito.",
+    "valor" : 150.00,
+    "tamanho" : "",   
+  }
+];
+
+
+router.get('/', function(req, res, next) {  
   var idprod = req.url;
   idprod = idprod.slice(2, 30);
-  titleprod = idprod;
-  idprod = "images/"+idprod+".jpg";
-  res.render('viewer', { title: titleprod, id: idprod });
+  for (const key in products) {
+    let prod = products[key];    
+    console.log(key);
+
+    for (const index in prod) {
+      console.log(`${index} : ${prod[index]}`);
+      if(idprod === prod.slug){
+        var nome = prod.name;
+        var imagem = prod.image;
+        var desc = prod.descricao;
+        var prec = prod.valor;
+        var slug = prod.slug;
+      }
+    }
+  }
+
+  console.log(nome,imagem,desc,prec);
+
+  res.render('viewer', { title: nome, image: imagem, description: desc, price: prec});
 });
 
 module.exports = router;
