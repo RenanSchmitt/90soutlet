01b43d102b4adc39506a72f3736f6b19608a3a18
RenanSchmitt <renan@dzestudio.com.br>
2018-08-10 15:58:33 -0300

    Muda lógica das thumbnails usando forEach

--- routes/products.js
+++ routes/products.js
@@ -6,26 +6,26 @@ const products = require('./../src/mock/db');
 router.get('/', function(req, res, next) {
   // res.render('products', { title: 'Express' , id: "" });
   
-  let nome;
-  let imagem;
-  let desc;
-  let prec;
-  let slug;
-  let tam;
+  // let nome;
+  // let imagem;
+  // let desc;
+  // let prec;
+  // let slug;
+  // let tam;
 
-  for (const key in products) {
-    const prod = products[key];
+  // for (const key in products) {
+  //   const prod = products[key];
 
-    for (const index in prod) {
+  //   for (const index in prod) {
           
-        nome = prod.name;
-        imagem = prod.image;
-        desc = prod.descricao;
-        prec = prod.valor;
-        slug = prod.slug;
-        tam = prod.tamanho;   
-    }
-  }
+  //       nome = prod.name;
+  //       imagem = prod.image;
+  //       desc = prod.descricao;
+  //       prec = prod.valor;
+  //       slug = prod.slug;
+  //       tam = prod.tamanho;   
+  //   }
+  // }
   console.log(products);
   res.render('products', { prod_list: products });
 
